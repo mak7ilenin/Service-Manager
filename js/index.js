36940615f35c245c5207e66c6c1eeb0dd3645a5c
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-06-25 01:04:51 +0300

    Made a dropdown animation on services page (bad animation yet).

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $('.video_block').each(function() {
     $(this).contents().wrap('<a href="https://www.youtube.com/embed/dAGpVZbWxTk"></a>');
 });
+
+$('.top').click(function() {
+    $('.hidden_info').animate({
+        height: 'toggle'
+    }, "fast");
+    // $('.hidden_info').css('transform: translateY(-50%)');
+    // $('.hidden_info').toggle();
+});
